535e4e074683a0893c7278c903f86da40d77dcdc
sadik <po480373@gmai.com>
2022-04-17 13:42:10 +0600

    component routes and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 // import './App.css'
 
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import CheckOut from "./Pages/CheckOut/CheckOut";
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
+import Register from "./Pages/Register/Register";
+
 function App() {
-  return (<div className="App">
-    <h1>this is app</h1>
-  </div>);
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Header.css'
+const Header = () => {
+  return (
+    <nav style={{background:"black",padding:"1rem 0"}}>
+      <div style={{ display: "flex", justifyContent: "space-between",alignItems:"center", padding:"0 1rem" }}>
+        <div>
+          <Link className="link" to="/home">
+            Home
+          </Link>
+          <Link className="link" to="/checkout">
+            CheckOut
+          </Link>
+          <Link className="link" to="/about">
+            About
+          </Link>
+          <Link className="link" to="/blogs">
+            Blogs
+          </Link>
+        </div>
+        <div>
+          <Link className="link" to="/register">
+            Register
+          </Link>
+          <Link className="link" to="/login">
+            Login
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
